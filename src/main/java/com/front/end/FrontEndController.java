4b5e57accdbfe4529feee160ef12c9eb2677689d
chitralimbu <limbu_nikkil@hotmail.co.uk>
2019-10-30 13:48:33 +0000

    initial commit

---
+++ src/main/java/com/front/end/FrontEndController.java
@@ -0,0 +1,24 @@
+package com.front.end;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/time")
+public class FrontEndController {
+	
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+	
+	@GetMapping
+	public String getCurrentTime(Model model) {
+		LocalDateTime now = LocalDateTime.now();
+		String nowDate = now.format(formatter);
+		model.addAttribute("time", nowDate);
+		return "front";
+	}
+}
