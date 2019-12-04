a02b9e37c09ff6954817e863ba9c87c744a17462
chitralimbu <limbu_nikkil@hotmail.co.uk>
2019-12-04 16:41:36 +0000

    Tests added

--- src/main/java/com/front/end/FrontEndApplication.java
+++ src/main/java/com/front/end/FrontEndApplication.java
@@ -1,11 +1,28 @@
 package com.front.end;
 
+import com.front.end.domain.Person;
+import com.front.end.service.PersonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class FrontEndApplication {
 
+	@Bean
+	public List<Person> allPeople(){
+		return Arrays.asList(new Person("chitra", 24, "dev"),
+							new Person("bob", 34, "lol"));
+	}
+
+
+/*	@Bean
+	public PersonService personService(){
+		return new PersonService();
+	}*/
 	public static void main(String[] args) {
 		SpringApplication.run(FrontEndApplication.class, args);
 	}
--- src/main/java/com/front/end/ProcessingTimeLogInterceptor.java
+++ src/main/java/com/front/end/ProcessingTimeLogInterceptor.java
@@ -1,11 +1,13 @@
 package com.front.end;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class ProcessingTimeLogInterceptor implements HandlerInterceptor {
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
@@ -19,7 +21,7 @@ public class ProcessingTimeLogInterceptor implements HandlerInterceptor {
         String path = request.getRequestURL() + queryString;
         long startTime = (Long) request.getAttribute("startTime");
         long endTime = System.currentTimeMillis();
-        System.out.println(String.format("IP: %s - %s millisecond taken to process the request %s", request.getRemoteAddr(),(endTime - startTime), path));
+       log.info(String.format("IP: %s - %s millisecond taken to process the request %s", request.getRemoteAddr(),(endTime - startTime), path));
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exceptionIfAny){
